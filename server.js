5b0da5b23ec2bd22d7a6cd6dbba5499e2c3eb9a2
Martin Wan <mlwan@uvic.ca>
2016-06-16 11:47:51 -0700

    working board creation

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser")
+var app = express();
+
+app.use(express.static("public"));
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.listen(30144, function() {
+    console.log("Express listeningo on port 30144");
+}); 
+
+app.post("/newGame", function(req,res,next) {
+    console.log("Received request for new game", JSON.stringify(req.body));
+    res.write("Game #2");
+    res.end();
+    next();
+});
+
+app.post("/move", function(req,res,next) {
+    console.log("Received turn", JSON.stringify(req.body));
+    res.write("Received");
+    res.end();
+    next();
+});
