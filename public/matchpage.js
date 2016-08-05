4f9fef05a92354136cf4550a519429ebcbd33669
Martin Wan <mlwan@uvic.ca>
2016-08-05 13:38:25 -0700

    Update to matchmaking for user waiting for player to join. Forgot to push changes made on roberts server

--- public/matchpage.js
+++ public/matchpage.js
@@ -1,19 +1,23 @@
 window.onload = function() {
-    $.get('/game/matchstatus', function(data, status) {
-        if (data.redirect)
-            window.location = data.redirect;
-    });
+    longpoll();
 }
 
+var longpollActive = true;
 function longpoll() {
     $.ajax({
         method: 'GET',
         url: '/game/matchstatus',
         success: function(data) {
-            if (data.redirect)
+            if (data.redirect) {
+                longpollActive = false;
                 window.location = data.redirect;
+            }
         },
-        complete: longpoll,
+        complete: function(xhr, status) {
+                        if (status == "error" && longpollActive)
+                                window.alert("Error. Server disconnected")
+                        else longpoll();
+                },
         timeout: 30000
     });
 }
